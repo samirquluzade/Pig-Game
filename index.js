2fcb9c8640bee5e191a19955bc5f69ee4b6ad1a5
samirquluzade <63411203+samirquluzade@users.noreply.github.com>
2020-06-10 19:15:58 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var scores,roundScore,activePlayer,dice,roundScore1;
 init();
-    document.querySelector('#current-' + activePlayer).textContent = dice;
     document.querySelector('.btn-roll').addEventListener('click',function () {
     dice = Math.floor(Math.random() * 6) + 1;
     var diceDOM = document.querySelector('.dice');
